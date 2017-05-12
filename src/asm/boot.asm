763a83cb9f62f3b510dca416c70a74eef32736c8
Polly Nielsen Boutet-Livoff <pollyboutet@gmail.com>
2017-05-12 15:09:48 +0200

    add rust

--- src/asm/boot.asm
+++ src/asm/boot.asm
@@ -1,3 +1,4 @@
+extern kmain
 global start
 
 section .text
@@ -57,7 +58,7 @@ start:
 	mov es, ax	
 
 	; jump to long mode!
-	jmp gdt64.code:long_mode_start
+	jmp gdt64.code:kmain
 	
 	hlt
 
@@ -86,13 +87,3 @@ gdt64:
 .pointer:
     dw .pointer - gdt64 - 1
     dq gdt64
-
-; long mode
-section .text
-bits 64
-long_mode_start:
-
-	mov rax, 0x2f592f412f4b2f4f
-	mov qword [0xb8000], rax
-
-	hlt
